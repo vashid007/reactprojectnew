3298c68f1f75868e5f20692fcc880019c07070e4
tack and track <vashid.tackandtrack@gmail.com>
2026-02-16 20:48:34 +0400

    fourth version

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -3,23 +3,26 @@ import React from "react";
 function Auth() {
   return (
     <>
-      <div>
+      <div className="loginpage bg-primary py-4 py-md-5">
         <div className="container">
-          <h3>Back to home</h3>
-          <div className="row bg-primary d-flex ">
-            <div className="col">
+          <h3 className="mb-5 text-lite">Back to home</h3>
+          <div className="row m-2 p-4 bg-white p-3 p-md-5 g-5 align-items-center rounded-4">
+            <div className="col-12 col-md-5 d-flex justify-content-center align-items-center">
               <img
-                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fanimation%2F2023%2F06%2F13%2F15%2F12%2F15-12-30-710_512.gif&f=1&nofb=1&ipt=7bc7533e99279bc33abdab9dc63ce7c1adba42683b288631e6140363772568fd" className="w-50" alt=""
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fanimation%2F2023%2F06%2F13%2F15%2F12%2F15-12-30-710_512.gif&f=1&nofb=1&ipt=7bc7533e99279bc33abdab9dc63ce7c1adba42683b288631e6140363772568fd"
+                className="img-fluid"
+                style={{ maxWidth: "320px" }}
+                alt=""
               />
             </div>
 
-            <div className="col">
+            <div className="col-12 col-md-7">
               <h1>Project Vashid</h1>
-              <h1>Log in to your Account</h1>
-              <h1>Register your account</h1>
+              <h4 className="text-primary">Log in to your Account</h4>
+              <h4 className="text-primary">Register your account</h4>
 
               <form className="d-flex flex-column gap-3">
-                <div className="mb-3">
+                <div className="mb-1">
                   <label htmlFor="username" className="form-label">
                     User name
                   </label>
@@ -32,7 +35,7 @@ function Auth() {
                   />
                 </div>
 
-                <div className="mb-3">
+                <div className="mb-1">
                   <label htmlFor="email" className="form-label">
                     Email
                   </label>
@@ -44,7 +47,7 @@ function Auth() {
                   />
                 </div>
 
-                <div className="mb-3">
+                <div className="mb-1">
                   <label htmlFor="password" className="form-label">
                     Password
                   </label>
@@ -56,19 +59,19 @@ function Auth() {
                   />
                 </div>
 
-                <div>
+                <div className="d-flex align-items-center gap-3 flex-wrap">
                   <button type="submit" className="btn btn-secondary">
                     Log In
                   </button>
 
-                  <h5>Dont have a account REGISTER</h5>
+                  <h6>Dont have an account <span className="text-primary">REGISTER</span></h6>
                 </div>
 
-                <div>
+                <div className="d-flex align-items-center gap-3 flex-wrap">
                   <button type="submit" className="btn btn-secondary">
                     Register
                   </button>
-                  <h5>Already have a account LOG IN</h5>
+                  <h6>Already have an account <span className="text-primary">LOG IN </span></h6>
                 </div>
               </form>
             </div>
